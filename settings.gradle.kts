c3ec8112c2d8973d92cbe05006765736b461c673
Elly Kitoto <junkmailstoelly@gmail.com>
2026-07-08 11:13:45 +0300

    Scaffold workflow-demo Compose app module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,3 +28,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "kotlin-fhir-workflow"
 include(":workflow")
+include(":workflow-demo")
---
+++ workflow-demo/src/commonMain/kotlin/dev/ohs/fhir/workflow/demo/Placeholder.kt
@@ -0,0 +1,3 @@
+package dev.ohs.fhir.workflow.demo
+
+internal const val WORKFLOW_DEMO = "workflow-demo"
