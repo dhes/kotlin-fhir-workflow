82d1862e55704eda0443ec2efe7ca284395cfc6e
Elly Kitoto <junkmailstoelly@gmail.com>
2026-07-10 16:56:58 +0300

    Publish workflow library to Maven coordinates

--- workflow/build.gradle.kts
+++ workflow/build.gradle.kts
@@ -3,7 +3,12 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 plugins {
   alias(libs.plugins.kotlin.multiplatform)
   alias(libs.plugins.android.kotlin.multiplatform.library)
+  `maven-publish`
 }
+
+group = "dev.ohs.fhir"
+
+version = "1.0.0-alpha01"
 
 kotlin {
   jvmToolchain(21)
